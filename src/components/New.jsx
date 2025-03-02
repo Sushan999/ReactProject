3ac03f392cf856c58fa290a17e56cbfc9a995450
Sushan999 <thapasusan777@gmail.com>
2025-03-02 13:16:17 +0545

    h2

--- src/components/New.jsx
+++ src/components/New.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const New = () => {
-  return <div>My name is Sus</div>;
+  return (
+    <div>
+      My name is Sus<h2>Sus</h2>
+    </div>
+  );
 };
 
 export default New;
