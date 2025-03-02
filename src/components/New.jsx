d1209d35b02211580108f15f5137b0906e72463c
Sushan999 <thapasusan777@gmail.com>
2025-03-02 13:31:16 +0545

    Latest

--- src/components/New.jsx
+++ src/components/New.jsx
@@ -3,8 +3,9 @@ import React from "react";
 const New = () => {
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>Hey</h1>
       My name is Sus<h2>Sus</h2>
+      <h3>Hey</h3>
     </div>
   );
 };
